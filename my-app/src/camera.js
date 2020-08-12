7f3d2bc1683c2c3fd0624048ecf308fd3f217af6
klchow <klchow@gmail.com>
2020-08-13 00:34:55 +0800

    Camera fixed

--- my-app/src/camera.js
+++ my-app/src/camera.js
@@ -1,12 +1,55 @@
-console.log("aaa");// Grab elements, create settings, etc.
-var video = document.getElementById('video');
+const videoElement = document.querySelector('video');
+const videoSelect = document.querySelector('select#videoSource');
 
-// Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
-    });
+function gotDevices(deviceInfos) {
+    // Handles being called several times to update labels. Preserve values.
+    const value = videoSelect.value;
+
+    while (videoSelect.firstChild) {
+        videoSelect.removeChild(videoSelect.firstChild);
+    }
+
+    for (let i = 0; i !== deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i];
+        const option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'videoinput') {
+            option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+            videoSelect.appendChild(option);
+        }
+    }
+
+    if (Array.prototype.slice.call(videoSelect.childNodes).some(n => n.value === value)) {
+        videoSelect.value = value;
+    }
 }
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+function gotStream(stream) {
+    window.stream = stream; // make stream available to console
+    videoElement.srcObject = stream;
+    // Refresh button list in case labels have become available
+    return navigator.mediaDevices.enumerateDevices();
+}
+
+function handleError(error) {
+    console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+function start() {
+    if (window.stream) {
+        window.stream.getTracks().forEach(track => {
+            track.stop();
+        });
+    }
+    const videoSource = videoSelect.value;
+    const constraints = {
+        video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+    };
+    navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+}
+
+videoSelect.onchange = start;
+
+start();
