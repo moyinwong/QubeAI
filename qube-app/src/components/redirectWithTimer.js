706125c3a745b7879fdc3c8572017389a3ad8a31
klchow <klchow@gmail.com>
2020-08-15 21:46:38 +0800

    redirect timer function

--- qube-app/src/components/redirectWithTimer.js
+++ qube-app/src/components/redirectWithTimer.js
@@ -1,13 +1,16 @@
 const redirectWithTimer = (props, path, time) => {
     let timer;
+
     const setTimer = () => {
-        const timer = setTimeout(() => {
+        timer = setTimeout(() => {
             props.history.push(path);
         }, time)
     }
+
     const clearTimer = () => {
         clearTimeout(timer);
     }
+
     clearTimer();
     setTimer();
 }
