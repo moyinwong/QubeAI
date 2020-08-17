04e2840b55c5b9305e679a0ba7f55c2b862e816d
klchow <klchow@gmail.com>
2020-08-17 23:48:45 +0800

    Redux included

--- qube-app/src/index.js
+++ qube-app/src/index.js
@@ -4,8 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Provider store={store}><App /></Provider>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ qube-app/src/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    cubeArray: [
+        0, 0, 0, 0, 0, 0
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer;
