d864ca859a5848c029060f4972894c5a1f64db48
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-08-20 14:41:27 +0800

    minor fix

--- react-app/public/js/color-recognition.js
+++ react-app/public/js/color-recognition.js
@@ -46,7 +46,7 @@ function name_to_rgb(name) {
         'orange': [255, 165, 0, 255],
         'blue': [0, 0, 255, 255],
         'green': [0, 255, 0, 255],
-        'white': [255, 255, 255, 0],
+        'white': [255, 255, 255, 255],
         'yellow': [255, 255, 0, 255]
     }
     return color[name]
