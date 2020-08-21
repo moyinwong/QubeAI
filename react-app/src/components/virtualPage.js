3c223104b2bc8adf15a951f3e4001fece2767b50
Stanley Lau <siuclau4@gmail.com>
2020-08-22 00:30:16 +0800

    Adjustment for testing

--- react-app/src/components/virtualPage.js
+++ react-app/src/components/virtualPage.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import navBarAnimation from '../functions/navBarAnimation';
-import virtualModel from '../functions/virtualModel';
-import { changeState} from '../actions/changeState';
-import './virtualPage.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import navBarAnimation from "../functions/navBarAnimation";
+//import virtualModel from '../functions/virtualModel';
+import { changeState } from "../actions/changeState";
+import "./virtualPage.css";
 
 class VirtualPage extends Component {
-    componentDidMount() {
-        document.querySelector('.navBar').classList.remove('navBarHidden');
-        navBarAnimation("virtual");
-    }
-    render() {
-        return (
-            <div className="virtualPage">
-                <p>Virtual Page</p>
-                <div style={{width:'500px',height:'500px'}}>
-                    <div id="canvasVirtual">
-        
-                    </div>
-                </div>
-                
-                <div className="btnContainerVer">
-                    <button>Submit Cube</button>
-                </div>
-            </div>
-        )
-    }
+  componentDidMount() {
+    document.querySelector(".navBar").classList.remove("navBarHidden");
+    navBarAnimation("virtual");
+  }
+  render() {
+    return (
+      <div className="virtualPage">
+        <p>Virtual Page</p>
+        <div style={{ width: "500px", height: "500px" }}>
+          <div id="canvasVirtual"></div>
+        </div>
+
+        <div className="btnContainerVer">
+          <button>Submit Cube</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 // Connect to Redux Store
 const mapStateToProps = (state) => {
-    return { currentState: state.currentState };
-}
+  return { currentState: state.currentState };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        changeState: (currentState) => {
-            dispatch(changeState(currentState));
-        }
-    }
-}
+  return {
+    changeState: (currentState) => {
+      dispatch(changeState(currentState));
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(VirtualPage);
